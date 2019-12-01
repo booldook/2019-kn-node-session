feb973d057f8992dc6cef904a962e6515c106ba4
hee20192 <html20192@daum.net>
2019-12-01 16:02:30 +0900

    session 완료

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,33 @@ var { User, salt } = require("../models");
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.render('login');
+  if(req.session.user) res.send(`회원입니다.<p><a href="/users/logout">로그아웃</a></p>`);
+  else res.render('login');
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.user = null;
+  res.redirect("/users");
+});
+
+router.post('/login', async (req, res, next) => {
+  var result = await User.findOne({
+    where: {
+      userid: req.body.userid,
+      userpw: crypto.createHash('sha512').update(req.body.userpw + salt).digest('base64'),
+    }
+  });
+  if(result) {
+    // 로그인에 성공했으면...
+    req.session.user = {};
+    req.session.user.id = result.id;
+    req.session.user.userid = result.userid;
+    req.session.user.username = result.username;
+    res.redirect("/users");
+  }
+  else {
+    res.send("로그인에 실패했습니다.");
+  }
 });
 
 router.get('/join', (req, res, next) => {
@@ -18,7 +44,8 @@ router.post('/join', async (req, res, next) => {
     userpw: crypto.createHash('sha512').update(req.body.userpw + salt).digest('base64'),
     username: req.body.username,
   });
-  res.json(result);
+  if(result.id) res.redirect("/users");
+  else res.send("가입에 실패하였습니다.");
 })
 
 module.exports = router;
